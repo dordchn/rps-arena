824c9381a91f4698fc779bf4aefd7f15ab669ad3
Dor Cohen <dordchn@gmail.com>
2022-12-30 09:42:06 +0200

    Parameters editor

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-const CANVAS_SIZE = 680;
+const CANVAS_SIZE = 1400;
 const ITEM_TYPES = {
   ROCK: 0,
   PAPER: 1,
   SCISSORS: 2
 };
-const ITEM_SPEED = 0.1;
-const ITEM_RADIUS = 16;
-const ITEMS_COUNT = 30; // Items count of each type
+const params = {
+  itemsCount: 30,
+  itemRadius: 30,
+  itemSpeed: 0.2,
+}
 
 const canvas = document.querySelector('canvas');
 let items = [];
@@ -19,6 +21,8 @@ async function init() {
   canvas.width = CANVAS_SIZE;
   canvas.height = CANVAS_SIZE;
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+  loadParams();
 
   // load images
   const loadedPromises = ['./assets/rock.png', './assets/paper.png', './assets/scissors.png'].map((path, i) => {
@@ -33,16 +37,23 @@ async function init() {
   return Promise.all(loadedPromises);
 }
 
+function loadParams() {
+  params.itemsCount = Number(document.querySelector('#itemsCount').value);
+  params.itemRadius = Number(document.querySelector("#itemSize").value);
+  params.itemSpeed = Number(document.querySelector("#itemSpeed").value);
+
+}
+
 function generateItems(count) {
   items = [];
   for (var type of Object.keys(ITEM_TYPES)) {
     for (let j = 0; j < count; j++) {
       const direction = Math.random() * 2 * Math.PI;
       items.push({
-        x: ITEM_RADIUS + Math.random() * (CANVAS_SIZE - ITEM_RADIUS * 2),
-        y: ITEM_RADIUS + Math.random() * (CANVAS_SIZE - ITEM_RADIUS * 2),
-        vx: ITEM_SPEED * Math.sin(direction),
-        vy: ITEM_SPEED * Math.cos(direction),
+        x: params.itemRadius + Math.random() * (CANVAS_SIZE - params.itemRadius * 2),
+        y: params.itemRadius + Math.random() * (CANVAS_SIZE - params.itemRadius * 2),
+        vx: params.itemSpeed * Math.sin(direction),
+        vy: params.itemSpeed * Math.cos(direction),
         type: ITEM_TYPES[type],
       });
     }
@@ -56,8 +67,8 @@ function moveItems(dt) {
       x: item.x + item.vx * dt,
       y: item.y + item.vy * dt,
     }
-    if (predictedPos.x < ITEM_RADIUS || predictedPos.x > CANVAS_SIZE - ITEM_RADIUS) item.vx = -item.vx;
-    if (predictedPos.y < ITEM_RADIUS || predictedPos.y > CANVAS_SIZE - ITEM_RADIUS) item.vy = -item.vy;
+    if (predictedPos.x < params.itemRadius || predictedPos.x > CANVAS_SIZE - params.itemRadius) item.vx = -item.vx;
+    if (predictedPos.y < params.itemRadius || predictedPos.y > CANVAS_SIZE - params.itemRadius) item.vy = -item.vy;
 
     item.x += item.vx * dt;
     item.y += item.vy * dt;
@@ -87,14 +98,14 @@ function handleCollisions() {
 }
 
 function doesCollide(itemA, itemB) {
-  return Math.hypot(itemA.x - itemB.x, itemA.y - itemB.y) < ITEM_RADIUS * 2;
+  return Math.hypot(itemA.x - itemB.x, itemA.y - itemB.y) < params.itemRadius * 2;
 }
 
 function render() {
   const context = canvas.getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
   items.forEach(item => {
-    context.drawImage(ITEMS_IMG[item.type], item.x - ITEM_RADIUS, item.y - ITEM_RADIUS, ITEM_RADIUS * 2, ITEM_RADIUS * 2);
+    context.drawImage(ITEMS_IMG[item.type], item.x - params.itemRadius, item.y - params.itemRadius, params.itemRadius * 2, params.itemRadius * 2);
   });
 }
 
@@ -109,7 +120,15 @@ function mainLoop() {
 
 (async () => {
   await init();
-  generateItems(ITEMS_COUNT);
+  generateItems(params.itemsCount);
   prevLoopTime = Date.now();
   mainLoop();
 })();
+
+document.querySelector('button').addEventListener('click', evt => {
+  items = [];
+  loadParams();
+  setTimeout(function () {
+    generateItems(params.itemsCount);
+  }, 300);
+});
